e893d65b0cfe6c45ffd925a81298b397150c5415
contact390 <contact@hitaishicorp.com>
2026-02-05 12:46:06 +0530

    meassage

--- db.js
+++ db.js
@@ -1,15 +1,12 @@
 const mysql = require('mysql2/promise');
 
-const mysql = require("mysql2/promise");
-
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "opticsuser",
-  password: "StrongPass@123",
-  database: "hitaishilens",
+  host: 'localhost',
+  user: 'root',
+  password: '2001',      // add your MySQL password
+  database: 'hitaishilens',
   waitForConnections: true,
   connectionLimit: 10
 });
 
-module.exports = pool;
 module.exports = pool;
